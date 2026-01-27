a036df6b7d63e4024001b99704bf90467ce91c9b
MANSUORMADE <mansuorktmt8@gmail.com>
2026-01-27 01:02:04 -0800

    error4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,17 +10,17 @@ import Home from './page/Home/Home.js';
 import Footer from './componat/footer/Footer.js';
 import Nows from './page/nows/nows.js';
 import Aside from "./componat/aside/Aside.js"
-// import AboutMessage from './page/message/aboutMessage.js';
-// import Messages from './page/message/message.js';
+import AboutMessage from './page/message/aboutMessage.js';
+import Messages from './page/message/message.js';
 
 import Login from './page/auth/login.js';
 import Register from './page/auth/Register.js';
 import Reast from './page/auth/Reast.js';
 import ReastPassword from './page/auth/reastpassword.js';
 // // start about to Store 
-// import About from './page/about/About';
-// import TheAbout from './componat/aboutStore/About.js';
-// import Dotoget from './componat/aboutStore/Dotoget.js';
+import About from './page/about/About';
+import TheAbout from './componat/aboutStore/About.js';
+import Dotoget from './componat/aboutStore/Dotoget.js';
 // End about to Store 
 // // start Account User 
 // import AboutAccount from './page/account/aboutAccount.js';
@@ -68,7 +68,6 @@ const App = () => {
         <Aside />
         <div className='content'>
         <Outlet />
-
         </div>
         <Footer  />
       </div>
@@ -120,32 +119,32 @@ const App = () => {
         path: "/:notFound/:not",
         element: <Notfound />
       },
-      // {
-      //   path: "/about",
-      //   element: <About />,
-      //   children: [
-      //     {
-      //       path: "messages",
-      //       element: <AboutMessage />
-      //     },
-      //     {
-      //       path: "",
-      //         element: <TheAbout />
-      //       },
-      //        {
-      //           path: ":notFound",
-      //           element: <Notfound />
-      //         },
-      //       {
-      //         path: "messages",
-      //         element: <Messages />
-      //       },
-      //       {
-      //         path: "do-to-get",
-      //         element: <Dotoget />
-      //       },
-      //     ]
-      // },
+      {
+        path: "/about",
+        element: <About />,
+        children: [
+          {
+            path: "messages",
+            element: <AboutMessage />
+          },
+          {
+            path: "",
+              element: <TheAbout />
+            },
+             {
+                path: ":notFound",
+                element: <Notfound />
+              },
+            {
+              path: "messages",
+              element: <Messages />
+            },
+            {
+              path: "do-to-get",
+              element: <Dotoget />
+            },
+          ]
+      },
       // {
       //   path: "/account",
       //   element: <AboutAccount />,
